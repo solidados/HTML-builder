4ac0e97cb41f9d0c24f27117d5fd727d423aeb2a
solidados <geekbrains.pk@internet.ru>
2023-05-05 17:36:37 +0400

    refactor: task 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,8 @@
-const { readFile } = require('node:fs');
+const fs = require('node:fs');
 const { resolve } = require('node:path');
+const { stdout } = process;
 
 const filePath = resolve(__dirname, 'text.txt');
-
-readFile(filePath, 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const readStream = new fs.createReadStream(filePath, 'utf8');
+readStream.on('err', err => stdout.write(err));
+readStream.pipe(stdout);
