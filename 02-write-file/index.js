ad67c604b13eda14d62e880b2e35b747e5dac899
solidados <geekbrains.pk@internet.ru>
2023-05-07 00:58:05 +0400

    refactor: edit task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,8 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const readline = require('node:readline');
-const { mkdir, writeFile, appendFile, readFile, rename } = fs;
-const { stdin, stdout, exit } = process;
+const { stdout, exit } = process;
 
 const {
   stdin: input,
@@ -10,38 +9,33 @@ const {
 } = require('node:process');
 
 const filePath = path.join(__dirname, 'newFile.txt');
-/* if (!filePath) {
-  writeFile(filePath, err => { if (err) throw err });
-} else {
-  appendFile(filePath, err => { if (err) throw err });
-} */
 const fileText = fs.createWriteStream(filePath);
 const rl = readline.createInterface({ input, output });
 
 readline.emitKeypressEvents(input);
 
-if (input.isTTY)
-  input.setRawMode(true);
-input.on('keypress', (chunk, key) => {
-  if (key && key.ctrl && key.name === 'c') {
-    stdout.write('\nProcess was terminated by user\n');
-    exit();
-  }
-});
-
-const write = () => {
+const newEntry = () => {
   rl.question('List your favourite movies?\nTo terminate send \'exit\' or Ctrl+C\n> ',
     (answer) => {
-      if (answer.toLocaleLowerCase() === 'exit') {
+      if (answer.toLowerCase() === 'exit') {
         stdout.write('Process was terminated by user\n');
         rl.close();
         exit();
-      }
+      } else if (input.isTTY) input.setRawMode(true);
+      input.on('keypress', (chunk, key) => {
+        if (key && key.ctrl && key.name === 'c') {
+          stdout.write('\nProcess was terminated by user\n');
+          rl.close();
+          exit();
+        }
+      });
       fileText.write(`${answer}\n`, err => {
-        if (err) console.log(err.message);
-        write();
+        if (err) throw err/* stdout.write(err.message) */;
+        newEntry();
       })
     })
 };
-process.on('exit', () => { stdout.write('Have a nice day...\n'); });
-write();
+process.on('exit', () => {
+  stdout.write('Have a nice day...\n');
+});
+newEntry();
