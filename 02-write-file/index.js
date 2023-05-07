84db5475ddf9fe9833fc22f39bf773fce1fb19d3
solidados <geekbrains.pk@internet.ru>
2023-05-07 17:30:07 +0400

    refactor: final edit of task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,41 +1,40 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const readline = require('node:readline');
-const { stdout, exit } = process;
 
 const {
   stdin: input,
   stdout: output,
+  exit: exit,
 } = require('node:process');
 
-const filePath = path.join(__dirname, 'newFile.txt');
+const filePath = path.join(__dirname, 'myFilms.txt');
 const fileText = fs.createWriteStream(filePath);
 const rl = readline.createInterface({ input, output });
-
-readline.emitKeypressEvents(input);
 
 const newEntry = () => {
   rl.question('List your favourite movies?\nTo terminate send \'exit\' or Ctrl+C\n> ',
     (answer) => {
       if (answer.toLowerCase() === 'exit') {
-        stdout.write('Process was terminated by user\n');
+        output.write('Process was terminated by user\n');
         rl.close();
         exit();
-      } else if (input.isTTY) input.setRawMode(true);
-      input.on('keypress', (chunk, key) => {
-        if (key && key.ctrl && key.name === 'c') {
-          stdout.write('\nProcess was terminated by user\n');
-          rl.close();
-          exit();
-        }
-      });
+      }
       fileText.write(`${answer}\n`, err => {
         if (err) throw err;
         newEntry();
       })
     })
 };
-process.on('exit', () => {
-  stdout.write('Have a nice day...\n');
+// Ctrl+C implementation:
+rl.on('SIGINT', () => {
+  rl.close(
+    output.write('\nProcess was terminated by user\n')
+  );
 });
+
+process.on('exit', () => {
+  output.write('Have a nice day...\n');
+});
+
 newEntry();
