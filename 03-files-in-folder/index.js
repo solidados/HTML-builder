f90f911a17a8337b2506f9f2c9e4d4ab56772742
solidados <geekbrains.pk@internet.ru>
2023-05-07 19:42:26 +0400

    refactor: edit task 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,22 @@
 const fs = require('fs');
 const path = require('path');
-
-const { readdir, stat } = fs;
-const { join, resolve, parse } = path;
 const { stdout } = process;
 
 const folderContent = () => {
-  const folderPath = join(__dirname, 'secret-folder');
+  const folderPath = path.join(__dirname, 'secret-folder');
 
-  readdir(folderPath, (err, files) => {
+  fs.readdir(folderPath, (err, files) => {
     if (err) throw err;
 
     files.forEach((file) => {
-      stat(resolve(folderPath, file), (err, Stats) => {
+      fs.stat(path.resolve(folderPath, file), (err, Stats) => {
         if (err) throw err;
 
         if (Stats.isFile()) {
-          const fileName = parse(file).name;
-          const fileExtension = parse(file).ext.slice(1);
+          const fileName = path.parse(file).name;
+          const fileExtension = path.parse(file).ext.slice(1);
           const fileSize = (Stats.size / 1024).toFixed(2);
-          stdout.write(`${fileName} - ${fileExtension} - ${fileSize}kb\n`)
+          stdout.write(`${fileName} - ${fileExtension} - ${fileSize}kb\n`);
         }
       })
     })
