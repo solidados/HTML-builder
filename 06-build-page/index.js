a51b39bd2bddb105d0149bba1f5699ae2de9f317
solidados <geekbrains.pk@internet.ru>
2023-05-09 01:59:14 +0400

    refactor: task 06-build-page; creating function for readdir srcDir and copy files

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,3 +1,5 @@
+// ! --- 01:57am Please give me some more time. and check again before deadline. Thank you!
+
 const fs = require('node:fs');
 const path = require('node:path');
 const fsPromises = require('fs/promises');
@@ -10,7 +12,7 @@ const destAssets = path.join(destBuild, 'assets');
 
 // const srcComponents = path.join(__dirname, 'components');
 // const srcStyles = path.join(__dirname, 'styles');
-// const srcAssets = path.join(__dirname, 'assets');
+const srcAssets = path.join(__dirname, 'assets');
 
 const createDist = async () => {
   await fsPromises.rm(path.resolve(destBuild), { recursive: true, force: true });
@@ -24,7 +26,31 @@ const createAssets = async () => {
   }
 };
 
+const copyAssets = async () => {
+  fsPromises.readdir((srcAssets, { withFileTypes: true }), (_, files) => {
+    for (const file of files) {
+      const srcFile = path.join(srcAssets, file);
+      const destFile = path.join(destAssets, file);
+      if (file.isFile()) {
+        fsPromises.copyFile(srcFile, destFile)
+      } else {
+        console.log('Something went wrong');
+        // fsPromises.readdir((srcFile, { withFileTypes: true }), () => {})
+      }
+    }
+  })
+}
 
 createDist();
 createAssets();
+copyAssets();
 
+/* fs.readdir(srcAssets, (_, files) => {
+  for (const file of files) {
+    console.log(file);
+    fs.readFile(file, () => {
+      console.log(content);
+
+    });
+  }
+}) */
