3ba86577cefb0ff939f3e5ec2b688d76f1d8b9e6
solidados <geekbrains.pk@internet.ru>
2023-05-09 01:22:27 +0400

    refactor: task 06-build-page; edit async functions to create project folders

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,25 +3,28 @@ const path = require('node:path');
 const fsPromises = require('fs/promises');
 
 const destBuild = path.join(__dirname, 'project-dist');
+const destAssets = path.join(destBuild, 'assets');
+
 // const buildLayout = path.join(buildFolder, 'index.html');
 // const buildStyles = path.join(buildFolder, 'style.css');
 
-const srcComponents = path.join(__dirname, 'components');
-const srcStyles = path.join(__dirname, 'styles');
-const srcAssets = path.join(__dirname, 'assets');
+// const srcComponents = path.join(__dirname, 'components');
+// const srcStyles = path.join(__dirname, 'styles');
+// const srcAssets = path.join(__dirname, 'assets');
 
 const createDist = async () => {
   await fsPromises.rm(path.resolve(destBuild), { recursive: true, force: true });
-  await fsPromises.mkdir(destBuild, console.log('Project folder was created'));
 };
-createDist();
 
 const createAssets = async () => {
   try {
-    await fsPromises.access(destBuild);
-    await fsPromises.mkdir(destBuild, 'assets');
+    await fsPromises.mkdir(destAssets, { recursive: true });
   } catch (error) {
-    console.error(Error);
+    console.error(error);
   }
 };
+
+
+createDist();
 createAssets();
+
