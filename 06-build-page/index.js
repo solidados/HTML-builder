774162a3e9ebaed800c860a3de4b5bd20e119011
solidados <geekbrains.pk@internet.ru>
2023-05-09 00:57:25 +0400

    refactor: edit task 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,12 +2,26 @@ const fs = require('node:fs');
 const path = require('node:path');
 const fsPromises = require('fs/promises');
 
-const buildFolder = path.join(__dirname, 'project-dist');
-const buildLayout = path.join(buildFolder, 'index.html');
-const buildStyles = path.join(buildFolder, 'style.css');
+const destBuild = path.join(__dirname, 'project-dist');
+// const buildLayout = path.join(buildFolder, 'index.html');
+// const buildStyles = path.join(buildFolder, 'style.css');
 
-fsPromises.mkdir(buildFolder, () => {
-  console.log('Build folder created');
-});
+const srcComponents = path.join(__dirname, 'components');
+const srcStyles = path.join(__dirname, 'styles');
+const srcAssets = path.join(__dirname, 'assets');
 
+const createDist = async () => {
+  await fsPromises.rm(path.resolve(destBuild), { recursive: true, force: true });
+  await fsPromises.mkdir(destBuild, console.log('Project folder was created'));
+};
+createDist();
 
+const createAssets = async () => {
+  try {
+    await fsPromises.access(destBuild);
+    await fsPromises.mkdir(destBuild, 'assets');
+  } catch (error) {
+    console.error(Error);
+  }
+};
+createAssets();
