1aa9df2d2cb9a8e0ba945ae201daef9087f8b8d3
solidados <geekbrains.pk@internet.ru>
2023-05-08 21:37:39 +0400

    refactor: edit task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,4 +1,3 @@
-const { finished } = require('node:stream/promises');
 const {
   createReadStream,
   createWriteStream,
@@ -8,7 +7,6 @@ const {
 } = require('node:fs');
 const { readdir } = require('node:fs/promises');
 const { join, resolve, extname } = require('node:path');
-const { stdout } = process;
 
 const src = join(__dirname, 'styles');
 const dest = join(__dirname, 'project-dist', 'bundle.css');
@@ -19,22 +17,21 @@ const readFolderContent = async () => {
   const files = await readdir(src);
   for (const file of files) {
     const srcFilePath = join(src, file);
-    
+
     try {
       stat(resolve(srcFilePath), (_, stats) => {
         if (stats.isFile() && extname(file) == '.css') {
           createReadStream(srcFilePath, 'utf-8');
-          
+  
           readFile(srcFilePath, 'utf-8', (err, data) => {
             if (err) throw err;
             appendFile(dest, data, err => {
               if (err) throw err;
-              stdout.write(`${file} - data was transfered\n`);
+              console.log(`${file} - data was transfered`);
             });
           });
         };
       });
-      await finished(rs);
     } catch (error) {
       console.error(error);
     }
